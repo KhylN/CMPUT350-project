965d844fd123e5d3846a383cb9ef27d0747dae34
Khyl Nad <khylanad@gmail.com>
2024-10-23 23:59:06 -0600

    testing

--- BasicSc2Bot.cpp
+++ BasicSc2Bot.cpp
@@ -1,5 +1,7 @@
 #include "BasicSc2Bot.h"
 
-void BasicSc2Bot::OnGameStart() { return; }
+void BasicSc2Bot::OnGameStart() {
+  std::cout << "Game Started! Let's collect some resources!" << std::endl;
+}
 
 void BasicSc2Bot::OnStep() { return; }
