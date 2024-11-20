f9250069e2df4b8339bd31db24cb638bbf3cc0af
Kouta MacQuarrie <157745603+kmacquarrie@users.noreply.github.com>
2024-11-19 23:50:26 -0700

    Update BasicSc2Bot.h

--- BasicSc2Bot.h
+++ BasicSc2Bot.h
@@ -3,18 +3,18 @@
 
 #include "sc2api/sc2_api.h"
 #include "sc2api/sc2_args.h"
+#include "sc2api/sc2_unit_filters.h" // needed for IsUnit
 #include "sc2lib/sc2_lib.h"
-#include "sc2utils/sc2_manage_process.h"
 #include "sc2utils/sc2_arg_parser.h"
+#include "sc2utils/sc2_manage_process.h"
 
 class BasicSc2Bot : public sc2::Agent {
 public:
 	virtual void OnGameStart();
 	virtual void OnStep();
+  virtual void OnUnitIdle(const sc2::Unit *unit);
 
 private:
-<<<<<<< Updated upstream
-=======
   void ManageSupply();
   void ManageSCVs();
   void ManageTroopsAndBuildings();
@@ -34,9 +34,7 @@ private:
   void ManageFactory();
 
   const sc2::Unit *FindNearestMineralPatch(const sc2::Point2D &start);
-  void TrainStarportUnits();
   void ForceSCVsToBuildAndHarvest();
->>>>>>> Stashed changes
 };
 
 #endif
