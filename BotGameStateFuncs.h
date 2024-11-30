a6d878385ad6974788c459ae26a417177a1c1073
Kouta MacQuarrie <157745603+kmacquarrie@users.noreply.github.com>
2024-11-30 01:58:29 -0700

    Update BotGameStateFuncs.h

--- BotGameStateFuncs.h
+++ BotGameStateFuncs.h
@@ -1,3 +1,6 @@
+#ifndef BOT_GAME_STATE_FUNCS
+#define BOT_GAME_STATE_FUNCS
+
 void InitializeEnemyLocations();
 int CountUnits(sc2::UNIT_TYPEID unit_type);
 sc2::Point2D GetBaseLocation();
@@ -6,3 +9,5 @@ const sc2::Unit *FindNearestVespene(const sc2::Point2D &start);
 int MilitaryStrength();
 std::vector<sc2::Point2D> potential_enemy_locations_;
 size_t current_attack_wave_ = 1;
+
+#endif
